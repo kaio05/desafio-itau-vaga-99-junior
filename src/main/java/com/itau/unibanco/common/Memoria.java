934673c7d12bc5ccad7c95943cf46ac1e568ffd6
Kaio Silva <kaiofernando05cont@gmail.com>
2025-02-21 10:13:25 -0300

    feat: create memory bean

---
+++ src/main/java/com/itau/unibanco/common/Memoria.java
@@ -0,0 +1,19 @@
+package com.itau.unibanco.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.ArrayList;
+import java.util.List;
+import com.itau.unibanco.models.Transacao;
+
+@Configuration
+public class Memoria {
+
+    List<Transacao> transacoes = new ArrayList<>();
+
+    @Bean
+    public Memoria memoria() {
+        return new Memoria();
+    }
+    
+}
