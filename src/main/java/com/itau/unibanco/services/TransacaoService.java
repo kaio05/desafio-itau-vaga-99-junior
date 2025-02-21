13c39cd34121fa5081bb55d851529994449c6dd5
Kaio Silva <kaiofernando05cont@gmail.com>
2025-02-21 10:31:51 -0300

    feat: create TransacaoService

---
+++ src/main/java/com/itau/unibanco/services/TransacaoService.java
@@ -0,0 +1,19 @@
+package com.itau.unibanco.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itau.unibanco.common.Memoria;
+import com.itau.unibanco.models.Transacao;
+
+@Service
+public class TransacaoService {
+
+    @Autowired
+    private Memoria memoria;
+
+    public void criarTransacao(Transacao transacao) {
+        memoria.salvarTransacao(transacao);
+    }
+
+}
