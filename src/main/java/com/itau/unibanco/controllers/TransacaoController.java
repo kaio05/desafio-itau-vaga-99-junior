dad4bb84bfd4c76c549e255464b007c41bf6af7a
Kaio Silva <kaiofernando05cont@gmail.com>
2025-03-01 07:08:35 -0300

    feat: add delete transactions to controller

--- src/main/java/com/itau/unibanco/controllers/TransacaoController.java
+++ src/main/java/com/itau/unibanco/controllers/TransacaoController.java
@@ -3,6 +3,7 @@ package com.itau.unibanco.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +25,11 @@ public class TransacaoController {
     public ResponseEntity<Transacao> criarTransacao(@Valid @RequestBody Transacao transacao) {
         transacaoService.criarTransacao(transacao);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Transacao> excluirTransacoes() {
+        transacaoService.apagarMemoria();;
+        return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
