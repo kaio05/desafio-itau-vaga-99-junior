48b2b235246dfa4c8cd9b17e6a563e5f93aafa18
Kaio Silva <kaiofernando05cont@gmail.com>
2025-03-02 21:29:49 -0300

    refactor: change transaction controller bad request error

--- src/main/java/com/itau/unibanco/controllers/TransacaoController.java
+++ src/main/java/com/itau/unibanco/controllers/TransacaoController.java
@@ -3,13 +3,14 @@ package com.itau.unibanco.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.itau.unibanco.models.Transacao;
+import com.itau.unibanco.common.dtos.TransacaoDto;
 import com.itau.unibanco.services.TransacaoService;
 
 import jakarta.validation.Valid;
@@ -22,13 +23,16 @@ public class TransacaoController {
     private TransacaoService transacaoService;
 
     @PostMapping
-    public ResponseEntity<Transacao> criarTransacao(@Valid @RequestBody Transacao transacao) {
+    public ResponseEntity<TransacaoDto> criarTransacao(@Valid @RequestBody TransacaoDto transacao, BindingResult bindingResult) {
+        if(bindingResult.hasFieldErrors())
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            
         transacaoService.criarTransacao(transacao);
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
     @DeleteMapping
-    public ResponseEntity<Transacao> excluirTransacoes() {
+    public ResponseEntity<TransacaoDto> excluirTransacoes() {
         transacaoService.apagarMemoria();;
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
