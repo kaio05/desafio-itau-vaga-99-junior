61957fa9cd07d6a1376dfc52d15f975749e71254
Kaio Silva <kaiofernando05cont@gmail.com>
2025-03-05 10:43:40 -0300

    test: create tests for memory

---
+++ src/test/java/com/itau/unibanco/common/MemoriaTest.java
@@ -0,0 +1,40 @@
+package com.itau.unibanco.common;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.OffsetDateTime;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.itau.unibanco.common.dtos.TransacaoDto;
+
+public class MemoriaTest {
+
+    @Autowired
+    Memoria memoria = new Memoria();
+
+    @Test
+    @DisplayName("Should show that the memory is empty")
+    void testApagarMemoria() {
+        TransacaoDto transacao = new TransacaoDto(10.0, OffsetDateTime.now());
+
+        memoria.salvarTransacao(transacao);
+
+        memoria.apagarMemoria();
+
+        assertThat(memoria.getLista().isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should show that one transaction was saved")
+    void testSalvarTransacao() {
+        TransacaoDto transacao = new TransacaoDto(10.0, OffsetDateTime.now());
+
+        memoria.salvarTransacao(transacao);
+
+        assertEquals(memoria.getLista().size(), 1);
+    }
+}
